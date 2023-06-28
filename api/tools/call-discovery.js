acc6a199dec1b93c7f557c00bf8da52ced7bd8cd
ega4432 <38056766+ega4432@users.noreply.github.com>
2023-06-28 13:48:01 +0900

    update discovery api to v2

--- api/tools/call-discovery.js
+++ api/tools/call-discovery.js
@@ -1,8 +1,8 @@
-const DiscoveryV1 = require('ibm-watson/discovery/v1');
+const DiscoveryV2 = require('ibm-watson/discovery/v2');
 const {IamAuthenticator} = require('ibm-watson/auth');
 const config = require('config');
 
-const discovery = new DiscoveryV1({
+const discovery = new DiscoveryV2({
   version: config.get('watson.discovery.version'),
   authenticator: new IamAuthenticator({
     apikey: config.get('watson.discovery.apikey'),
@@ -10,37 +10,35 @@ const discovery = new DiscoveryV1({
   serviceUrl: config.get('watson.discovery.serviceUrl'),
 });
 
-const createQuery = (categoryLabel, searchStr) => {
-  const texts = searchStr.split(' ').map((item) => `text:"${item}"`).join(',');
-  return `enriched_text.categories.label::"${categoryLabel}",(${texts})`;
-};
+// const createQuery = (categoryLabel, searchStr) => {
+//   const texts = searchStr.split(' ').map((item) => `text:"${item}"`).join(',');
+//   return `enriched_text.categories.label::"${categoryLabel}",(${texts})`;
+// };
 
-const runQuery = async (categoryLabel, searchStr) => {
-  const query = createQuery(categoryLabel, searchStr);
+const runQuery = async (searchStr) => {
+  // const query = createQuery(categoryLabel, searchStr);
 
   const queryParams = {
-    environmentId: config.get('watson.discovery.environmentId'),
-    collectionId: config.get('watson.discovery.collectionId'),
-    highlight: true,
-    query,
-    _return: 'highlight',
+    projectId: config.get('watson.discovery.projectId'),
+    collectionIds: [config.get('watson.discovery.collectionId')],
+    query: searchStr,
   };
 
-  console.log(`Running query - ${query}`);
+  console.log(`Running query - ${searchStr}`);
   const queryResponse = await discovery.query(queryParams);
 
   if (queryResponse.result.results && queryResponse.result.results.length > 0) {
-    return queryResponse.result.results[0].highlight.text[0]
-        .replace(/<em>/g, '')
-        .replace(/<\/em>/g, '');
+    return queryResponse.result.results.map((result, index) => {
+      return {id: index + 1, name: result.extracted_metadata.title};
+    });
   } else {
-    return '該当する情報が見つかりませんでした。';
+    return [];
   }
 };
 
 const exec = async () => {
   try {
-    const responseText = await runQuery('/health and fitness/disease', 'コロナ 症状');
+    const responseText = await runQuery('/health and fitness/disease', 'コロナ');
 
     console.log(responseText);
   } catch (err) {
